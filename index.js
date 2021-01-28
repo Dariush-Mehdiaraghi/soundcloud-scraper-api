ebac597bafffcb810016dcc677dfb49026bf82f9
Dariush <dariush007@gmx.ch>
2021-01-28 16:52:56 +0100

    first test

--- index.js
+++ index.js
@@ -3,23 +3,29 @@ const express = require('express');
 const app = express();
 const SoundCloud = require("soundcloud-scraper");
 const client = new SoundCloud.Client();
-
+const fs = require("fs")
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
 });
 
 app.get('/', (req, res) => {
-  console.log("request was made");
+    console.log("request was made");
 
-  client
-    .getSongInfo(
-      "https://soundcloud.com/shelter12kollektiv/sheltercast-no6-schematica"
-    )
-    .then(async (song) => {
-      res.json(song);
-    })
-    .catch(console.error);
+    client
+        .getSongInfo(
+            "https://soundcloud.com/shelter12kollektiv/dariush-mjs-kassette"
+        )
+        .then(async (song) => {
+            const stream = await song.downloadProgressive();
+            //    const writer = stream.pipe(fs.createWriteStream(`./${song.title}.mp3`));
+            res.set('content-disposition', `attachment; filename="${song.title}.mp3"`);
+            stream.pipe(res);
+
+           
+           // res.json(song);
+        })
+        .catch(console.error);
 
 
 
